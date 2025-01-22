6c0eea512ec9550b0f596aa760648053649543a2
mahantha <swamymahan280@gmail.com>
2025-01-22 23:36:02 +0530

    object and class attributes

--- class_objects.py
+++ class_objects.py
@@ -15,3 +15,20 @@ c1 = Car("TATA",2019)
 c2 = Car("AUDI",2020)
 print(c1.name,c1.year)
 print(c2.name,c2.year)
+
+class office:
+    dept = "AIP"
+    def __init__(self,name,salary):
+        print("Office class constructor")
+        self.name = name;
+        self.salary = salary;
+
+c1 = office("mahan",250000)
+c2 = office("praveen",300000)
+
+print(f"name of collegue {c1.name} and salary is {c1.salary} belongs to dept {office.dept}")
+print(f"name of collegue {c2.name} and salary is {c2.salary} belongs to dept {c2.dept}")
+#here s1.name and s1.salary are object attributes as it will be differnt for each objects
+# where as dept is an class attribute as it same for all the objects
+# obj.attr  >>>> class.attr
+
